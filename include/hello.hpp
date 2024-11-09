84eba9aa311c5f31dcc3db130c6d877a73f09c15
floatpigeon <floatpigeon@proton.me>
2024-11-09 17:56:13 +0800

    Init

---
+++ include/hello.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+class hello {
+public:
+    void show() { std::cout << "hello" << std::endl; }
+};
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+
+#include <hello.hpp>
+
+int main() {
+    hello foo;
+    foo.show();
+
+    return 0;
+}
